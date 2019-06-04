d7ae31d148696dc6570ce5d5406c366b47b474e1
lucas <lsminteris@gmail.com>
2019-06-04 13:18:35 -0700

    updated index.js file to be lesson 3 code

--- lesson-03/index.js
+++ lesson-03/index.js
@@ -1,42 +1,23 @@
-/* eslint-disable prettier/prettier */
 'use strict'
 const Hapi = require('hapi')
 
 const server = new Hapi.Server()
-server.connection({
-  host: 'localhost',
-  port: 8000
-})
+server.connection({ port: 8000 })
 
-let goodOptions = {
-  reporters: [{
-    reporter: require('good-console'),
-    events: { log: ['error'], response: '*' }
-  }]
+function handler(request, reply){
+  reply(request.params)
 }
 
-server.register({
-  register: require('good'),
-  options: goodOptions
-}, err => {
-  server.route({
-    method: 'GET',
-    path: '/',
-    handler: (request, reply) => {
-      server.log('error', 'Oh no!')
-      server.log('info', 'replying')
-      reply('hello hapi')
-    }
-  })
-
-  server.route({
-    method: 'GET',
-    path: '/{name}',
-    handler: (request, reply) => {
-      reply(`hello ${request.params.name}`)
-    }
-  })
-
-  server.start(() => console.log(`Started at: ${server.info.uri}`))
-
+server.route({
+  method: 'GET',
+  path: '/{stuff*}',
+  handler: handler
 })
+
+server.route({
+  method: 'GET',
+  path: '/files/{file}.jpg',
+  handler: handler
+})
+
+server.start(() => console.log(`Started at: ${server.info.uri}`))
