1b55cfabb3b96129bd7323a085e4c0b9843bfcbf
lucas <lsminteris@gmail.com>
2019-06-06 11:56:30 -0700

    Duplicated lesson 5
    Duplicated lesson 5 and renamed it to lesson 6 so I can start with lesson 6

---
+++ lesson-06/index.js
@@ -0,0 +1,23 @@
+/* eslint-disable prettier/prettier */
+/* eslint-disable @typescript-eslint/no-var-requires */
+// eslint-disable-next-line prettier/prettier
+'use strict'
+const Hapi = require('hapi')
+const Boom = require('boom')
+
+const server = new Hapi.Server()
+server.connection({ port: 8000 })
+
+server.route({
+  method: 'GET',
+  path: '/',
+  handler: function(request, reply){
+    reply('hello world')
+      .code(418)
+      .type('text/plain')
+      .header('hello', 'world')
+      .state('hello', 'world')
+  }
+})
+
+server.start(() => {})
