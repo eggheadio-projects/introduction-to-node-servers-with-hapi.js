99a64cb2706039dec6392bbec7ee3c593abf81c3
lucas <lsminteris@gmail.com>
2019-06-11 12:58:37 -0700

    Start to lesson 8
    Edited index.js file to be the start of lesson 8 code.

--- lesson-08/index.js
+++ lesson-08/index.js
@@ -3,29 +3,15 @@
 // eslint-disable-next-line prettier/prettier
 'use strict'
 const Hapi = require('hapi')
-
 const server = new Hapi.Server()
 server.connection({ port: 8000 })
 
-server.register(require('vision'), () => {
-
-  server.views({
-    engines: {
-      hbs: require('handlebars')
-    },
-    relativeTo: __dirname,
-    layout: true,
-    path: 'views'
-  })
-
-  server.route({
-    method: 'GET',
-    path: '/{name?}',
-    handler: function(request, reply){
-      reply.view('home', { name: request.params.name || 'World'})
-    }
-  })
-
-  server.start(() => console.log(`Started at: ${server.info.uri}`))
+server.route({
+  method: 'GET',
+  path: '/',
+  handler: function(request, reply){
+    reply()
+  }
 })
 
+server.start(() => {})
