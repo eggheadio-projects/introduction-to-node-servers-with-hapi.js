74c72364f788e2513d2ee0c804736510423bfab0
lucas <lsminteris@gmail.com>
2019-07-03 14:35:30 -0700

    Lesson 12 code
    Start of lesson 12 code

--- lesson-12/index.js
+++ lesson-12/index.js
@@ -3,28 +3,15 @@
 // eslint-disable-next-line prettier/prettier
 'use strict'
 const Hapi = require('hapi')
-const Joi = require('joi')
 const server = new Hapi.Server()
 server.connection({ port: 8000 })
 
 server.route({
-  method: ['POST', 'PUT'],
-  path: '/user/{id?}',
+  method: 'GET',
+  path: '/',
   config: {
-    validate: {
-      params: Joi.object({
-        id: Joi.number()
-      }),
-      query: Joi.object({
-        id: Joi.number(),
-      })
-    },
     handler: function (request, reply) {
-      reply({
-        params: request.params,
-        query: request.query,
-        payload: request.payload
-      })
+      reply(`Cookies!`)
     }
   }
 })
