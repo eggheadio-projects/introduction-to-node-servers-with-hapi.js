5d1145b3da0b4ba56a164464702a9e8646ac5266
lucas <lsminteris@gmail.com>
2019-07-03 14:29:25 -0700

    Lesson 11 dupicated
    Duplicated lesson 11 and renamed it to lesson 12

---
+++ lesson-12/index.js
@@ -0,0 +1,32 @@
+/* eslint-disable prettier/prettier */
+/* eslint-disable @typescript-eslint/no-var-requires */
+// eslint-disable-next-line prettier/prettier
+'use strict'
+const Hapi = require('hapi')
+const Joi = require('joi')
+const server = new Hapi.Server()
+server.connection({ port: 8000 })
+
+server.route({
+  method: ['POST', 'PUT'],
+  path: '/user/{id?}',
+  config: {
+    validate: {
+      params: Joi.object({
+        id: Joi.number()
+      }),
+      query: Joi.object({
+        id: Joi.number(),
+      })
+    },
+    handler: function (request, reply) {
+      reply({
+        params: request.params,
+        query: request.query,
+        payload: request.payload
+      })
+    }
+  }
+})
+
+server.start(() => console.log(`Started at: ${server.info.uri}`))
