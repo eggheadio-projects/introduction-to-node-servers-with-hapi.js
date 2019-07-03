add145fc04d244ff61e97643b87f6316ee762154
lucas <lsminteris@gmail.com>
2019-07-03 14:16:24 -0700

    Start of lesson 11 code
    Edited index.js to be the start of the lesson 11 code

--- lesson-11/index.js
+++ lesson-11/index.js
@@ -3,32 +3,21 @@
 // eslint-disable-next-line prettier/prettier
 'use strict'
 const Hapi = require('hapi')
-const Boom = require('boom')
 const server = new Hapi.Server()
 server.connection({ port: 8000 })
-
-server.register(require('vision'), () => {
-  server.views({
-    engines: { hbs: require('handlebars') },
-    relativeTo: __dirname,
-    path: 'views'
-  })
-})
-
-server.ext('onPreResponse', (request, reply) => {
-  let resp = request.response
-  if(!resp.isBoom) return reply.continue() 
-
-  reply.view('error', resp.output.payload)
-    .code(resp.output.statusCode)
-})
 
 server.route({
   method: 'GET',
   path: '/',
-  handler: function (request, reply) {
-    reply(Boom.notFound())
+  config: {
+    handler: function (request, reply) {
+      reply({
+        params: request.params,
+        query: request.query,
+        payload: request.payload
+      })
+    }
   }
 })
 
-server.start(() => { })
+server.start(() => console.log(`Started at: ${server.info.uri}`))
