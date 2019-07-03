6adc5121c513a075b2794256b0fbeb1a555d2d95
lucas <lsminteris@gmail.com>
2019-07-03 14:11:25 -0700

    Finished lesson 10
    Copied lesson four over to lesson 10 since that was most like this lesson, added an error.hbs file to the views folder and all the code for that, finished lesson 10 code in index.js.

--- lesson-10/index.js
+++ lesson-10/index.js
@@ -3,22 +3,32 @@
 // eslint-disable-next-line prettier/prettier
 'use strict'
 const Hapi = require('hapi')
+const Boom = require('boom')
 const server = new Hapi.Server()
 server.connection({ port: 8000 })
 
-server.ext('onRequest', (request, reply)=> {
-  request.setUrl('/')
-  request.setMethod('GET')
-  reply.continue()
+server.register(require('vision'), () => {
+  server.views({
+    engines: { hbs: require('handlebars') },
+    relativeTo: __dirname,
+    path: 'views'
+  })
+})
+
+server.ext('onPreResponse', (request, reply) => {
+  let resp = request.response
+  if(!resp.isBoom) return reply.continue() 
+
+  reply.view('error', resp.output.payload)
+    .code(resp.output.statusCode)
 })
 
 server.route({
-  method: ['POST', 'PUT'],
+  method: 'GET',
   path: '/',
-  handler: function(request, reply){
-    console.log('handler')
-    reply('hello world')
+  handler: function (request, reply) {
+    reply(Boom.notFound())
   }
 })
 
-server.start(() => {})
+server.start(() => { })
