a68f7cdd338eb4dfeb799902fa24c2624f70690b
lucas <lsminteris@gmail.com>
2019-07-03 12:18:56 -0700

    lesson 9 duplicated, renamed 10
    This commit is just duplicating lesson 9 and renaming it to lesson 10 for the start of lesson 10.

---
+++ lesson-10/index.js
@@ -0,0 +1,24 @@
+/* eslint-disable prettier/prettier */
+/* eslint-disable @typescript-eslint/no-var-requires */
+// eslint-disable-next-line prettier/prettier
+'use strict'
+const Hapi = require('hapi')
+const server = new Hapi.Server()
+server.connection({ port: 8000 })
+
+server.ext('onRequest', (request, reply)=> {
+  request.setUrl('/')
+  request.setMethod('GET')
+  reply.continue()
+})
+
+server.route({
+  method: ['POST', 'PUT'],
+  path: '/',
+  handler: function(request, reply){
+    console.log('handler')
+    reply('hello world')
+  }
+})
+
+server.start(() => {})
