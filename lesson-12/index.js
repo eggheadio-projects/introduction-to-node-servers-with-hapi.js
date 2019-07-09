305ceddcf149cb3c3c99ff021c1460ae54450a5e
lucas <lsminteris@gmail.com>
2019-07-09 10:37:23 -0700

    Finished lesson 12
    Had to roll back node to v5.0.0 for this lesson to work. That was the main issue. Also, clearing the cookies got rid of the error that was giving me so much trouble.

--- lesson-12/index.js
+++ lesson-12/index.js
@@ -6,19 +6,21 @@ const Hapi = require('hapi')
 const server = new Hapi.Server()
 server.connection({ port: 8000 })
 
+server.state('hello', {
+  ttl: 60 * 60 * 1000,
+  isHttpOnly: true,
+  encoding: 'iron',
+  password: 'somethingSpecial'
+})
+
 server.route({
   method: 'GET',
   path: '/',
   config: {
     handler: function (request, reply) {
-      let hello = request.state.hello
+      let hello = request.state.hello.name
       reply(`Cookies! ${hello}`)
-        .state('hello', 'world', {
-          ttl: 60 * 60 * 1000,
-          isHttpOnly: true,
-          encoding: 'iron',
-          password: '123'
-        })
+        .state('hello', { name: 'Lucas' })
     }
   }
 })
